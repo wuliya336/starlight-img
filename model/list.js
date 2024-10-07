7b14949d7173948b034a3cbe4676228d5310f648
shiwuliya <3369906077@qq.com>
2024-10-07 21:07:17 +0800

    细节优化

--- model/list.js
+++ model/list.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-/**
- * 获取图片集列表
- * 
- * @returns {Promise<Array>} 返回包含图片集的列表数组
- */
 export async function getImageList() {
   try {
     const response = await axios.get('https://img.wuliya.icu/api/list', { timeout: 10000 });
--- model/randomImage.js
+++ model/randomImage.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 import { getImageList } from './index.js';
 
-/**
- * 根据名称或随机获取图片集
- * @param {string} name - 图片集名称，可选。如果为空，则随机选择一个图片集。
- * @returns {Promise<Object>} 返回找到的图片集。
- */
 async function getImageCollection(name = '') {
   const dataList = await getImageList();
 
@@ -20,12 +15,6 @@ async function getImageCollection(name = '') {
   return dataList[Math.floor(Math.random() * dataList.length)];
 }
 
-/**
- * 根据给定图片集的别名获取图片
- * @param {string} alias - 图片集别名
- * @param {number} count - 图片数量
- * @returns {Promise<Array>} 返回图片数组
- */
 async function fetchImages(alias, count) {
   const apiUrl = `https://img.wuliya.icu/api/${alias}?type=json&num=1`;
   const requests = Array.from({ length: count }, () => axios.get(apiUrl, { timeout: 10000 }));
@@ -36,13 +25,7 @@ async function fetchImages(alias, count) {
     .flatMap(response => response.value.data.images);
 }
 
-/**
- * 获取随机图片或指定图片集的图片，支持并行处理多个请求。
- * 
- * @param {string} name - 图片集名称，可选。如果为空，则随机选择一个图片集。
- * @param {number} count - 需要的图片数量，默认为 1。
- * @returns {Object} 包含请求状态码和图片列表。
- */
+
 export async function getRandomImages(name = '', count = 1) {
   const selectedItem = await getImageCollection(name);
 
