e70131d208415b021595988246ba5c5fc453bdc0
shiwuliya <3369906077@qq.com>
2024-10-08 17:37:40 +0800

    fix

--- apps/update.js
+++ apps/update.js
@@ -21,53 +21,50 @@ export class update extends plugin {
       ]
     });
   }
-  
+
   async initUpdate(e, Plugin_Name, customRemoteUrl) {
     const updateInstance = new Update(e);  
 
-    return new Proxy(updateInstance, {
-      get(target, prop) {
-        if (prop === "getRemoteUrl") {
-          return () => {
-            console.log(`getRemoteUrl called, returning custom URL: ${customRemoteUrl}`);
-            return customRemoteUrl;
-          };
-        }
 
-        if (prop === "getLog") {
-          return async (...args) => {
-            console.log(`Proxy intercepted getLog for plugin: ${Plugin_Name}`);
+    updateInstance.getRemoteUrl = () => {
+      console.log(`getRemoteUrl called, returning custom URL: ${customRemoteUrl}`);
+      return customRemoteUrl; 
+    };
 
-            const log = await target.getLog(...args);
-            if (log && log.data) {
-              log.data = log.data.map(node => ({
-                ...node,
-                nickname: "星点图片",  
-                user_id: Bot.uin,     
-              }));
-            }
 
-            return log; 
-          };
-        }
+    updateInstance.getLog = async (...args) => {
+      console.log(`getLog called for plugin: ${Plugin_Name}`);
 
-        return target[prop];  
+
+      const log = await Update.prototype.getLog.apply(updateInstance, args);
+
+      if (log && log.data) {
+        log.data = log.data.map(node => ({
+          ...node,
+          nickname: "星点图片",
+          user_id: Bot.uin,    
+        }));
       }
-    });
+
+      return log;
+    };
+
+    return updateInstance;
   }
 
   async update(e = this.e) {
-    if (!this.e.isMaster) return false; 
+    if (!this.e.isMaster) return false;  
     e.msg = `#${e.msg.includes("强制") ? "强制" : ""}更新starlight-img`; 
 
-    const up = await this.initUpdate(e, Plugin_Name, customRemoteUrl); 
+    const up = await this.initUpdate(e, Plugin_Name, customRemoteUrl)
     up.e = e; 
 
-    return up.update();
+    return up.update(); 
   }
 
+
   async sendLog(e = this.e) {
-    if (!this.e.isMaster) return false;  
+    if (!this.e.isMaster) return false;
 
     const up = await this.initUpdate(e, Plugin_Name, customRemoteUrl);  
     up.e = e; 
@@ -82,7 +79,6 @@ export class update extends plugin {
       ...node,
       message: node.message 
     }));
-
     return e.reply(await Bot.makeForwardMsg(updatedNodes));
   }
 }
