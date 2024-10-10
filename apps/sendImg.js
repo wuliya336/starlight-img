c35a6ce4f0d46813c65edb0439e5f682ce323da9
shiwuliya <3369906077@qq.com>
2024-10-11 02:24:47 +0800

    fix

--- apps/sendImg.js
+++ apps/sendImg.js
@@ -65,7 +65,7 @@ export class sendImg extends plugin {
         }
       }
     } else {
-      await e.reply('获取API数据失败或图片名称${name}不存在');
+      await e.reply(`获取API数据失败或图片名称${name}不存在`);
     }
   }
 }
